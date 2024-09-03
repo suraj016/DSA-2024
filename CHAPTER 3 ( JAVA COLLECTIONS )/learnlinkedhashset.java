9073ea4410b18d2a40f188b1d7c88177a23bab37
Suraj Kumar <suraj.kr1450@gmail.com>
2024-09-03 13:44:29 +0530

    chapter 4 startred

--- CHAPTER 3 ( JAVA COLLECTIONS )/learnlinkedhashset.java
+++ CHAPTER 3 ( JAVA COLLECTIONS )/learnlinkedhashset.java
@@ -1,38 +1,39 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+// import java.util.HashSet;
+// import java.util.LinkedHashSet;
+// import java.util.Set;
+// import java.util.TreeSet;
+// //import java.util.LearnSet;
 
-public class LearnSet {
+// public class LearnLinkedHashSet {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
        
-       Set<Integer> set = new LinkedHashSet<>();
+//        Set<Integer> set = new LinkedHashSet<>();
 
 
 
-       set.add(32);
-       set.add(2);
-       set.add(54);
-       set.add(21);
-       set.add(65);
+//        set.add(32);
+//        set.add(2);
+//        set.add(54);
+//        set.add(21);
+//        set.add(65);
 
-       System.out.println(set);
+//        System.out.println(set);
 
-       set.remove(54);
+//        set.remove(54);
 
-       System.out.println(set);
+//        System.out.println(set);
 
-       System.out.println(set.contains(21));
+//        System.out.println(set.contains(21));
 
-       System.out.println(set.isEmpty());
+//        System.out.println(set.isEmpty());
 
-       System.out.println(set.size());
+//        System.out.println(set.size());
 
-       set.clear();
+//        set.clear();
 
-        System.out.println(set);
+//         System.out.println(set);
 
-    }
-}
+//     }
+// }
