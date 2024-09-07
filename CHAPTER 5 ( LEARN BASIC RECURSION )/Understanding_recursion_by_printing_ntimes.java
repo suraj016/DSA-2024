b33fd70dce5baee1cfadde156471c0dc7216b8fc
Suraj Kumar <suraj.kr1450@gmail.com>
2024-09-07 19:16:16 +0530

    recursion started

---
+++ CHAPTER 5 ( LEARN BASIC RECURSION )/Understanding_recursion_by_printing_ntimes.java
@@ -0,0 +1,20 @@
+/*
+WHAT IS RECURSION
+-> when a fuction call itself 
+   until a specified condition is meet.
+
+
+*/ 
+
+void f(){
+    printf(1);
+    f();
+}
+
+int main(int argc, char const *argv[])
+{
+    f();
+   
+}
+
+
